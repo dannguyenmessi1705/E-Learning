9fdbb578c0ebea8914ce3e1c37a047ae079f8842
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-09-16 00:07:50 +0700

    feat: material service

--- be/materials/src/main/java/com/didan/elearning/materials/MaterialsApplication.java
+++ be/materials/src/main/java/com/didan/elearning/materials/MaterialsApplication.java
@@ -1,11 +1,39 @@
 package com.didan.elearning.materials;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAware")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Microservice for managing materials of e-learning",
+				description = "This microservice is responsible for managing materials of e-learning",
+				version = "${info.app.version}",
+				contact = @Contact(
+						name = "Nguyễn Di Đan",
+						url = "https://iam.didan.id.vn",
+						email = "didannguyen17@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.apache.org/licenses/LICENSE-2.0.html"
+				),
+				summary = "Microservice for managing materials of e-learning"
+		),
+		servers = {
+				@Server(
+						description = "Local server",
+						url = "http://localhost:9040"
+				)
+		}
+)
 public class MaterialsApplication {
 
 	public static void main(String[] args) {
---
+++ be/materials/src/main/java/com/didan/elearning/materials/dto/response/GeneralResponse.java
@@ -0,0 +1,34 @@
+package com.didan.elearning.materials.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Schema(
+    name = "GeneralResponse",
+    description = "The general response body"
+)
+public class GeneralResponse<T> {
+
+  @Schema(
+      name = "statusCode",
+      description = "The status code of the response",
+      example = "200"
+  )
+  private Integer statusCode;
+  @Schema(
+      name = "message",
+      description = "The message of the response",
+      example = "Success"
+  )
+  private String message;
+  @Schema(
+      name = "data",
+      description = "The data of the response"
+  )
+  private T data;
+}
